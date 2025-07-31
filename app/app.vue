227aa7c8907adfabeef7a6a60b9a36c53d0cc434
Omar <o.rizk@mybrite.ai>
2025-07-31 17:21:02 -0400

    sync

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="container px-4 py-4">
+  <div class="flex justify-between px-4 py-4">
     <div class="flex items-center gap-4">
       <img src="/img/profile-image.jpg" alt="profile image" class="w-[150px] h-[150px] rounded-full object-cover"/>
       <h1 class="text-[120px] leading-none font-bold">Omar Rezk</h1>
+    </div>
+    <div class="flex items-center">
+      <ul class="flex gap-4">
+        <a href="#"><li class="text-[50px] leading-none font-bold">About</li></a>
+        <a href="#"><li class="text-[50px] leading-none font-bold">Experiences</li></a>
+        <a href="#"><li class="text-[50px] leading-none font-bold">Projects</li></a>
+      </ul>
     </div>
   </div>
 </template>
