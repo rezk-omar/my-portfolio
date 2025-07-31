d27dca47f221e55fbb037b816d7320833832339d
Omar <o.rizk@mybrite.ai>
2025-07-31 17:09:31 -0400

    working on navbar

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container px-4 py-4">
-    <h1 class="text-3xl">Hello World</h1>
+    <div class="flex items-center gap-4">
+      <img src="/img/profile-image.jpg" alt="profile image" class="w-[150px] h-[150px] rounded-full object-cover"/>
+      <h1 class="text-[120px] leading-none font-bold">Omar Rezk</h1>
+    </div>
   </div>
 </template>
 
